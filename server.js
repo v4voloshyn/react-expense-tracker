22de822960a08da5731730f3b09cc6cfc7b86590
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-04-20 12:54:54 +0300

    prepare to build

--- server.js
+++ server.js
@@ -23,6 +23,12 @@ if(process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/transactions', transactions)
 
+if(process.env.NODE_ENV === 'production') {
+   app.use(express.static('client/build'));
+
+   app.get('*', (request, response) => response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} Mode on ${PORT} Port`.cyan.bold))
